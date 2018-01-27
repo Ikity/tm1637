56a7d2faae606f2d25314b960a741a673b2fc917
Ikity <ikity99@gmail.com>
2018-01-27 22:26:30 +0500

    updating example

--- examples/TM1637Test/TM1637Test.ino
+++ examples/TM1637Test/TM1637Test.ino
@@ -1,112 +1,105 @@
-#include <Arduino.h>
 #include <TM1637Display.h>
+#define CLK 13
+#define DIO 14
 
-// Module connection pins (Digital Pins)
-#define CLK 2
-#define DIO 3
 
-// The amount of time (in milliseconds) between tests
 #define TEST_DELAY   2000
 
 const uint8_t SEG_DONE[] = {
-	SEG_B | SEG_C | SEG_D | SEG_E | SEG_G,           // d
-	SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F,   // O
-	SEG_C | SEG_E | SEG_G,                           // n
-	SEG_A | SEG_D | SEG_E | SEG_F | SEG_G            // E
-	};
+  SEG_B | SEG_C | SEG_D | SEG_E | SEG_G,           // d
+  SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F,   // O
+  SEG_C | SEG_E | SEG_G,                           // n
+  SEG_A | SEG_D | SEG_E | SEG_F | SEG_G            // E
+  };
 
-TM1637Display display(CLK, DIO);
+void setup() {
+  // put your setup code here, to run once:
+TM1637_Display(CLK, DIO);
 
-void setup()
-{
 }
 
-void loop()
-{
-  int k;
+void loop() {
+  // put your main code here, to run repeatedly:
+int k;
+  bool lz = false;
   uint8_t data[] = { 0xff, 0xff, 0xff, 0xff };
-  display.setBrightness(0x0f);
-
-  // All segments on
-  display.setSegments(data);
+  TM1637_setBrightness(0x0f,1);
+   // All segments on
+  
+  TM1637_setSegments(data,4,0);
   delay(TEST_DELAY);
 
-  // Selectively set different digits
   data[0] = 0b01001001;
-  data[1] = display.encodeDigit(1);
-  data[2] = display.encodeDigit(2);
-  data[3] = display.encodeDigit(3);
+  data[1] = TM1637_encodeDigit(1);
+  data[2] = TM1637_encodeDigit(2);
+  data[3] = TM1637_encodeDigit(3);
 
   for(k = 3; k >= 0; k--) {
-	display.setSegments(data, 1, k);
-	delay(TEST_DELAY);
-	}
-
-  display.setSegments(data+2, 2, 2);
+  TM1637_setSegments(data, 1, k);
   delay(TEST_DELAY);
-
-  display.setSegments(data+2, 2, 1);
-  delay(TEST_DELAY);
-
-  display.setSegments(data+1, 3, 1);
-  delay(TEST_DELAY);
-
-
-  // Show decimal numbers with/without leading zeros
-  bool lz = false;
-  for (uint8_t z = 0; z < 2; z++) {
-	for(k = 0; k < 10000; k += k*4 + 7) {
-		display.showNumberDec(k, lz);
-		delay(TEST_DELAY);
-	}
-	lz = true;
   }
 
-  // Show decimal number whose length is smaller than 4
-  for(k = 0; k < 4; k++)
-	data[k] = 0;
-  display.setSegments(data);
+  TM1637_setSegments(data+2, 2, 2);
+  delay(TEST_DELAY);
 
-	// Run through all the dots
-	for(k=0; k <= 4; k++) {
-		display.showNumberDecEx(0, (0x80 >> k), true);
-		delay(TEST_DELAY);
-	}
+  TM1637_setSegments(data+2, 2, 1);
+  delay(TEST_DELAY);
 
-  display.showNumberDec(153, false, 3, 1);
+  TM1637_setSegments(data+1, 3, 1);
   delay(TEST_DELAY);
-  display.showNumberDec(22, false, 2, 2);
+
+lz=false;
+  for (uint8_t z = 0; z < 2; z++) {
+  for(k = 0; k < 10000; k += k*4 + 7) {
+   // TM1637_showNumberDec(k,0,lz,uint8_t length = 4,uint8_t pos = 0);
+    TM1637_showNumberDec(k, lz, 4,0);
+    delay(TEST_DELAY);
+  }
+  lz = true;
+  }
+for(k = 0; k < 4; k++)
+  data[k] = 0;
+  TM1637_setSegments(data,4,0);
+
+  // Run through all the dots
+  for(k=0; k <= 4; k++) {
+    TM1637_showNumberDecEx(0, (0x80 >> k), true,4,0);
+    delay(TEST_DELAY);
+  }
+TM1637_showNumberDec(153, false, 3, 1);
   delay(TEST_DELAY);
-  display.showNumberDec(0, true, 1, 3);
+  TM1637_showNumberDec(22, false, 2, 2);
   delay(TEST_DELAY);
-  display.showNumberDec(0, true, 1, 2);
+  TM1637_showNumberDec(0, true, 1, 3);
   delay(TEST_DELAY);
-  display.showNumberDec(0, true, 1, 1);
+  TM1637_showNumberDec(0, true, 1, 2);
   delay(TEST_DELAY);
-  display.showNumberDec(0, true, 1, 0);
+  TM1637_showNumberDec(0, true, 1, 1);
+  delay(TEST_DELAY);
+  TM1637_showNumberDec(0, true, 1, 0);
   delay(TEST_DELAY);
 
   // Brightness Test
   for(k = 0; k < 4; k++)
-	data[k] = 0xff;
+  data[k] = 0xff;
   for(k = 0; k < 7; k++) {
-    display.setBrightness(k);
-    display.setSegments(data);
+   TM1637_setBrightness(k,1);
+    TM1637_setSegments(data,4,0);
     delay(TEST_DELAY);
-  }
-  
-  // On/Off test
-  for(k = 0; k < 4; k++) {
-    display.setBrightness(7, false);  // Turn off
-    display.setSegments(data);
-    delay(TEST_DELAY);
-    display.setBrightness(7, true); // Turn on
-    display.setSegments(data);
-    delay(TEST_DELAY);  
   }
 
-  // Done!
-  display.setSegments(SEG_DONE);
+   for(k = 0; k < 4; k++) {
+    TM1637_setBrightness(7, false);  // Turn off
+    TM1637_setSegments(data,4,0);
+    delay(TEST_DELAY);
+    TM1637_setBrightness(7, true); // Turn on
+    TM1637_setSegments(data,4,0);
+    delay(TEST_DELAY);  
+
+      // Done!
+ TM1637_setSegments(SEG_DONE,4,0);
 
   while(1);
+  }
+
 }
